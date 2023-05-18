c8d09ce1a6b9685322ffdb7ebafa06064cebcd74
Oscar Jacobsen <67653883+oscarjacobsen@users.noreply.github.com>
2023-05-18 16:19:57 +0200

    123

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://oscarjacobsen.io"
+          href="https://oscarjacobsen.no"
           target="_blank"
           rel="noopener noreferrer"
         >
