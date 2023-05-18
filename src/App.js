6ae705df16fbc430d74ed78fa5f230836a883bef
Oscar Jacobsen <67653883+oscarjacobsen@users.noreply.github.com>
2023-05-18 16:05:02 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome all you <code>Digital Engineers!</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://oscarjacobsen.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My homepage
         </a>
       </header>
     </div>
